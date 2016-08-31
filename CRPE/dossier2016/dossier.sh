535d07faf3c23e3b406972f144fcea49040366cf
LibreEdu <pachot@gmail.com>
2016-08-31 23:31:18 +0200

    2016-08-31

---
+++ CRPE/dossier2016/dossier.sh
@@ -0,0 +1,10 @@
+pdflatex laGerminationPSMS
+bibtex laGerminationPSMS
+pdflatex laGerminationPSMS
+pdflatex laGerminationPSMS
+pdflatex dossier
+rm *.aux *.bak *.log *.out *.toc *.synctex.gz *.bbl *.bbg
+#git add *.tex
+#git add *.pdf
+#git commit -m "2015-02-11"
+#git push
